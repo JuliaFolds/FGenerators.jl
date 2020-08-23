a2e91a04e51a8f134413d21289c9db0aee15481d
Takafumi Arakaki <aka.tkf@gmail.com>
2020-08-22 19:13:46 -0700

    Add documentation for at-yieldfrom

--- src/FGenerators.jl
+++ src/FGenerators.jl
@@ -54,6 +54,27 @@ end
 
 """
     @yieldfrom foldable
+
+Yield items from a `foldable`.  This is usable only inside special
+contexts such as within [`@fgenerator`](@ref) macro.
+
+## Examples
+
+```jldoctest @yieldfrom
+julia> using FGenerators
+
+julia> @fgenerator function flatten2(xs, ys)
+           @yieldfrom xs
+           @yieldfrom ys
+       end;
+
+julia> collect(flatten2(1:2, 11:12))
+4-element Array{Int64,1}:
+  1
+  2
+ 11
+ 12
+```
 """
 macro yieldfrom(args...)
     _, on_yieldfrom = YIELD_DEF[]
